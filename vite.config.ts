d59d351a732952acafa4cc00365faf2c55f58ee6
Pen Menghong <penmenghong7@gmail.com>
2025-08-24 22:54:33 +0700

    Deploy portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,21 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
     
+  },build: {
+    // Increase chunk size warning limit
+    chunkSizeWarningLimit: 2000,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split React into its own chunk
+          react: ["react", "react-dom"],
+          // Split three.js (used by spline)
+          three: ["three"],
+          // You can add more heavy libs if needed
+        },
+      },
+    },
   },
   base: "/Portfolio/"
   
