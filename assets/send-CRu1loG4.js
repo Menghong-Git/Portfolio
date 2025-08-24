59d6f1b71f4ec37d5fd387ecb44cd8c131686e3b
Pen Menghong <penmenghong7@gmail.com>
2025-08-24 23:13:53 +0700

    Updates

--- assets/send-CRu1loG4.js
+++ assets/send-CRu1loG4.js
@@ -1,4 +1,4 @@
-import{c as a}from"./createLucideIcon-CeN92hwb.js";/**
+import{c as a}from"./createLucideIcon-_GsEL3BG.js";/**
  * @license lucide-react v0.536.0 - ISC
  *
  * This source code is licensed under the ISC license.
