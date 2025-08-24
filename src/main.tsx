25a7ec02f77cf823d7036580ac68a29817c8927c
Pen Menghong <penmenghong7@gmail.com>
2025-08-24 23:14:05 +0700

    Pportfolio change

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AllRoutes from "./Routes";
-import { HashRouter } from "react-router";
+import { BrowserRouter} from "react-router";
 
 
 createRoot(document.getElementById("root")!).render(
-    <HashRouter basename="/Portfolio">
+    <BrowserRouter basename="/Portfolio">
       <AllRoutes />
-    </HashRouter>
+    </BrowserRouter>
 );
