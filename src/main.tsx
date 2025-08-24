0c6523ec21659c74f7b82a8efcf3df719949ce02
Pen Menghong <penmenghong7@gmail.com>
2025-08-24 23:09:38 +0700

    Pportfolio change

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AllRoutes from "./Routes";
 import { HashRouter } from "react-router";
-import React from "react";
+
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <HashRouter basename="/Portfolio">
       <AllRoutes />
     </HashRouter>
-  </React.StrictMode>
 );
