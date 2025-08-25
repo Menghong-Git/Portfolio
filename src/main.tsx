eda255a0d3641fd81ec67e38ed58a1a8fcdc63a1
Pen Menghong <penmenghong7@gmail.com>
2025-08-25 14:02:08 +0700

    Deploy in vite

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter} from "react-router-dom";
 
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename="/Portfolio">
+    <BrowserRouter basename="/">
       <AllRoutes />
     </BrowserRouter>
 );
