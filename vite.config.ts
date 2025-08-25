bb18de723aebf1cce402ae88ec06dc455825dc24
Pen Menghong <penmenghong7@gmail.com>
2025-08-25 13:41:22 +0700

    fix vite vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,23 +10,6 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    
-  },build: {
-    // Increase chunk size warning limit
-    chunkSizeWarningLimit: 2000,
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Split React into its own chunk
-          react: ["react", "react-dom"],
-          // Split three.js (used by spline)
-          three: ["three"],
-          // You can add more heavy libs if needed
-        },
-      },
-    },
   },
-  base: "/Portfolio/"
-  
-})
+  base: "./",
+});
